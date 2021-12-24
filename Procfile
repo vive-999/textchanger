007ca4d46cc3d96422e9755c72f9605aebb07911
Vivek Raj <raj.vivek999@gmail.com>
2021-12-24 14:46:21 +0530

    made changes for deployment on heroku:

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: waitress-serve --port=$PORT Textchanger.wsgi:application
--- Textchanger/settings.py
+++ Textchanger/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,12 +21,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-87zzla39uvm3ux22#yqd&ymhw=w+)y!v%dm%s*6iwm8b)af1%7'
+SECRET_KEY = os.environ['SECRET_KEY_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["textchangervive.herokuapp.com","localhost"]
 
 
 # Application definition
@@ -41,6 +42,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -123,3 +125,5 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+STATIC_URL ='/static/'
+STATIC_ROOT = BASE_DIR / 'static'  # a folder is created on heroku and static files is stored in this folder. USe whitenoise for this
